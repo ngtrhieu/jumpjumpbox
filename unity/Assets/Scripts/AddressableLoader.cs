e7f72c1313f36ec91d2d9293cc3e265f401e19dd
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-06 15:31:21 +0800

    feat(addressable): add red box via addressable

---
+++ unity/Assets/Scripts/AddressableLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableLoader : MonoBehaviour {
+
+    public AssetReferenceGameObject refGameObject;
+
+    void Start () {
+        refGameObject.InstantiateAsync (transform.position, transform.rotation);
+    }
+}
