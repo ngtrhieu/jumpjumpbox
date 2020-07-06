c1248bae9193572aa035e3cddf824463537fbd82
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-06 14:55:09 +0800

    ci(travis): fix build #1 failed due to script not found
    forgot to change dir of env.sh file after moving inside ./scripts/ci/steps/ folder
    
    build #1

--- scripts/ci/distribute.sh
+++ scripts/ci/distribute.sh
@@ -4,6 +4,6 @@ set -e
 
 dir=$(dirname ${BASH_SOURCE})
 
-source $dir/env.sh
+source $dir/steps/env.sh
 
 $dir/steps/app_distribution.sh
--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -4,7 +4,7 @@ set -e
 
 dir=$(dirname ${BASH_SOURCE})
 
-source $dir/env.sh
+source $dir/steps/env.sh
 
 $dir/steps/prepare_license.sh
 $dir/steps/test.sh
