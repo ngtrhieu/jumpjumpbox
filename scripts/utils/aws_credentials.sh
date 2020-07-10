6c6d3b02c96bfaecac2af6cd4863da3c769975a3
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-10 16:50:09 +0800

    ci(travis): setup aws_credentials for fastlane match

--- scripts/utils/aws_credentials.sh
+++ scripts/utils/aws_credentials.sh
@@ -8,8 +8,8 @@ mkdir -p ~/.aws
 
 cat >~/.aws/credentials <<EOL
 [default]
-aws_access_key_id = ${ARTIFACTS_KEY}
-aws_secret_access_key = ${ARTIFACTS_SECRET}
+aws_access_key_id = ${AWS_ACCESS_KEY}
+aws_secret_access_key = ${AWS_ACCESS_SECRET}
 EOL
 
 echo :: aws profile generated at ~/.aws/credentials
