4c45393b35db318345ba916f53667dd72edbb36c
ngtrhieu <ngtrhieu0011@gmail.com>
2020-07-14 14:41:49 +0800

    test(addressable): add test to ensure cube is loaded properly

---
+++ unity/Assets/Tests/Test_Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests {
+  public class PlayModeTest_Cube {
+
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+
+      SceneManager.LoadScene("Main");
+      Assert.Null(GameObject.FindGameObjectWithTag("Cube"));
+
+      yield return new WaitForSeconds(1);
+
+      Assert.NotNull(GameObject.FindGameObjectWithTag("Cube"));
+    }
+  }
+}
